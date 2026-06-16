a6510e200bc651f56af00ff89cd52dc89a151c61
Mohsen <Moohamadiann@gmail.com>
2026-06-17 02:27:14 +0330

    remove unnecessary files for this repo

--- commit_push.sh
+++
@@ -1,5 +0,0 @@
-git config --local user.email "moohamadiann@gmail.com"
-git config --local user.name "Mohsen"
-git add .
-git diff --quiet && git diff --staged --quiet || git commit -m "Add new songs"
-git push
